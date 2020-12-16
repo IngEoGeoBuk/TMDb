360b89db959f9897651747ce55ba2a285b07ea51
IngEoGeoBuk <59249899+IngEoGeoBuk@users.noreply.github.com>
2020-12-16 18:13:01 +0900

    Update Config.js

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,6 +2,6 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-export const API_KEY = 'b69ea62b350e9362b0e554f0b1a4dfb0';
+export const API_KEY = 'Put Your TheMovieDB's API_KEY';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 
